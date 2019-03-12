98a2820202d1529a832cdf594cf6074d958f91ff
snowpaaw <45407784+snowpaaw@users.noreply.github.com>
2019-03-12 15:43:46 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     if (gamePlaying){
         
         rollDice();
-     
         
     }
 });
@@ -61,8 +60,10 @@ function newGame(){
     document.getElementById('current-1').textContent = '0';
      document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
-    document.querySelector('.player-0-panel').classList.add('active');
+    
+    document.querySelector('.player-0-panel').classList.remove('active');
     document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
 }
 function rollDice(){
     diceDOM = document.querySelector('.dice');
